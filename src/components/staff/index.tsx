d2b55a4c6735ff76b7e358385558ec9ef3e4555d
Vijay <vijay.bapodara@bytestechnolab.in>
2022-06-21 19:11:07 +0530

    CSS Changes for Outlet Container

--- src/components/staff/index.tsx
+++ src/components/staff/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./index.module.css";
-import StaffCard from "./staffcard/StaffCard";
+import StaffCard from "./staffcard/staffcard";
 
 const index = (props: any) => {
 	return (
